9c27e60b9ac6837e6949e8aa1f4fcd3e3cc19213
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-30 16:54:09 +0300

    title is added to project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 //@ts-ignore
 import { Html, Head, Main, NextScript } from "next/document";
 
@@ -11,6 +12,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Kaushan+Script&family=Nunito:ital,wght@0,200;0,500;1,400&display=swap"
           rel="stylesheet"
         />
+        <title>Lutfullah Blog</title>
       </Head>
       <body className="bg-gradient-to-r from-green-default to-blue-500 dark:from-dark-200 dark:to-dark-100 dark:text-white">
         <Main />
