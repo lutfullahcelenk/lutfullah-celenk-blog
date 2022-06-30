bad8767eb74645655596f1642b103d2371aaf280
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-30 16:23:16 +0300

    dark colors are added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
     fontFamily: {
       kaushan: ["Kaushan Script"],
@@ -11,8 +12,15 @@ module.exports = {
       colors: {
         green: {
           default: "#00f260",
+        },
+        dark: {
+          DEFAULT: "#010101",
+          100: "#0a0b1e",
+          200: "#16181d",
+          500: "#0f1115",
+          700: "#202125"
         }
-      }
+      },
     },
   },
   plugins: [],
