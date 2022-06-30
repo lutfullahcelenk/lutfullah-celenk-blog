bb40f7e13e31ab9abe78abf6833c71d4c3821858
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-30 16:00:15 +0300

    bar component is created

---
+++ components/Bar/index.tsx
@@ -0,0 +1,20 @@
+import { FunctionComponent } from "react";
+import { ISkill } from "../../constants/type";
+
+const Bar: FunctionComponent<{ data: ISkill }> = ({
+  data: { Icon, level, name },
+}) => {
+  return (
+    <div className="my-2 text-white bg-gray-300 rounded-full">
+      <div
+        className="flex items-center px-4 py-2 rounded-full bg-gradient-to-r from-green-default to-blue-400"
+        style={{ width: level }}
+      >
+        <Icon className="mr-3" />
+        {name}
+      </div>
+    </div>
+  );
+};
+
+export default Bar;
