3e3d6089d3ed796046c34f849653fc886a53e7e1
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-07-01 00:45:54 +0300

    projects page is completed responsively

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import ProjectCard from "../components/ProjectCard/index";
+import ProjectNavbar from "../components/ProjectNavbar/index";
+import { projects as projectsData } from "../constants/data";
+import { Category } from "../constants/type";
 
 const Projects = () => {
-  return (
-    <div className="p-4">Projects</div>
-  )
-}
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState("all");
 
-export default Projects
+  const handleFilterCategory = (category: Category | "all") => {
+    if (category === "all") {
+      setProjects(projectsData);
+      setActive(category);
+      return;
+    }
+    const newArray = projectsData.filter((project) =>
+      project.category.includes(category)
+    );
+    setProjects(newArray);
+    setActive(category);
+  };
+
+  return (
+    <div className="px-5 py-2">
+      <ProjectNavbar active={active} handleFilterCategory={handleFilterCategory} />
+
+      <div className="relative grid grid-cols-12 gap-4 my-3">
+        {projects.map((project) => (
+          <div
+            key={project.name}
+            className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200"
+          >
+            <ProjectCard project={project} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
