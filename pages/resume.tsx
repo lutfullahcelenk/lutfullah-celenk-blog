34131aae38a6d90bafb45bfcc5e19942cb8d4850
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-11-23 14:55:53 +0300

    fix: update resume page for desing

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -15,7 +15,7 @@ const Resume = () => {
               Front End Development
             </h5>
             <p className="font-semibold">FrontEnd Development Program</p>
-            <p className="w-3/4 my-3 text-justify">
+            <p className="w-full sm:w-3/4 my-3 text-justify">
               I have graduated from the 9-month intense Front End Development
               Program in Virginia. I have a certificated in this field.{" "}
             </p>
@@ -25,7 +25,7 @@ const Resume = () => {
               Bachelors Degree
             </h5>
             <p className="font-semibold">Turkish Military Academy</p>
-            <p className="w-3/4 my-3 text-justify">
+            <p className="w-full sm:w-3/4 my-3 text-justify">
               I have graduated from Kuleli Military High School at 2011 and
               carried on my education on Turkish Military Acedemy and have
               graduated at 2015 as a Civil Engineer.
@@ -40,7 +40,7 @@ const Resume = () => {
               Front End Developer
             </h5>
             <p className="font-semibold">UPayments (August 2022 - on)</p>
-            <p className="w-3/4 my-3 text-justify">
+            <p className="w-full sm:w-3/4 my-3 text-justify">
               I am a part of a NextJs E-Commerce project as a mid level front
               end developer. I have used some technologies such as NextJS,
               TypeScript, Redux Toolkit, TailwindCSS, Husky, Cypress Testing
@@ -54,7 +54,7 @@ const Resume = () => {
             <p className="font-semibold">
               Westerops (September 2021 - August 2022)
             </p>
-            <p className="w-3/4 my-3 text-justify">
+            <p className="w-full sm:w-3/4 my-3 text-justify">
               I started in this company as a FrontEnd Developer and take parts
               on 7 on-live projects. I have used some methodologies and
               technologies about FrontEnd Development such as ReactJS,
